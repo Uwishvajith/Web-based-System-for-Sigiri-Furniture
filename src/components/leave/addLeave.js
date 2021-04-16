cd6926325542240999fb114c8838f21e3720c527
Kaveen LK <kmklaksitha@gmail.com>
2021-04-16 15:34:55 +0530

    leave management part

---
+++ src/components/leave/addLeave.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import { addLeave } from "../../services/leaveService";
+
+export default function AddLeave() {
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [days, setDays] = useState("");
+  const [reason, setReason] = useState("");
+
+  function sendData(e) {
+    e.preventDefault();
+
+    const newLeave = {
+      fName,
+      lName,
+      from,
+      to,
+      days,
+      reason,
+    };
+
+    addLeave(newLeave).then((response) => {
+      const message = response.ok
+        ? "Leave details insertion successful"
+        : "Failed to insert leave details";
+      alert(message);
+    });
+  }
+
+  return (
+    <div className="border bg-light my-5 mx-5">
+      <div className="my-5 mx-5">
+        <form onSubmit={sendData}>
+          <div className="row">
+            <div className="col-md-6">
+              <label for="fName">First name:</label>
+              <input
+                id="fName"
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                onChange={(e) => {
+                  setfName(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col-md-6">
+              <label for="lName">Last name:</label>
+              <input
+                id="lName"
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+                onChange={(e) => {
+                  setlName(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label for="dob">From:</label>
+              <input
+                id="from"
+                type="date"
+                className="form-control"
+                name="from"
+                onChange={(e) => {
+                  setFrom(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col">
+              <label for="dob">To:</label>
+              <input
+                id="to"
+                type="date"
+                className="form-control"
+                name="to"
+                onChange={(e) => {
+                  setTo(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col">
+              <label for="dob">Days:</label>
+              <input
+                id="days"
+                type="number"
+                className="form-control"
+                name="days"
+                onChange={(e) => {
+                  setDays(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <label for="fName">Reason:</label>
+              <textarea
+                className="form-control"
+                id="reason"
+                placeholder="reason"
+                rows="3"
+                onChange={(e) => {
+                  setReason(e.target.value);
+                }}
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col py-3 text-center">
+              <button type="submit" className="btn btn-primary">
+                add leave
+              </button>
+            </div>
+            <div className="col py-3 text-center">
+              <button type="reset" className="btn btn-primary">
+                reset
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/services/leaveService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const HOST = "http://localhost:4000";
+
+export const addLeave = async (leavePayload) => {
+  //console.log(leavePayload, "<<<<<<<<<<<<<<<<<<<<<<<<");
+  try {
+    await axios.post(`${HOST}/api/leave`, leavePayload);
+    return {
+      ok: true,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+    };
+  }
+};
+
+/*export const getAllleaves = async () => {
+  console.log("done;");
+  try {
+    const response = await axios.get(`${HOST}/api/leave`);
+    console.log(response, "res");
+    return {
+      ok: true,
+      data: response.data.data,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+    };
+  }
+};*/
