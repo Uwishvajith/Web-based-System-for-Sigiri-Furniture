998dda647b65eb26e8d0372551051985a6a24fdc
Uwishvajith <uwishvajith@gmail.com>
2021-04-21 12:15:36 +0530

    Dashboard update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,7 +11,7 @@ function Dashboard(props) {
               <div class="col-sm">
                 <a href="/empList">
                 <button type="button" class=" dbutton" id="btn-dash">
-                  <i class="fa fa-group"  id="fa fa-2x"></i>
+                  <i class="fa fb fa-group"  id="fa fa-2x"></i>
                   <span class="lead align-top ">Employee Management</span>
                 </button>
                 </a>
@@ -20,7 +20,7 @@ function Dashboard(props) {
               <a href="/viewVehicle">
                 <button type="button" class="dbutton" id="btn-dash">
                   
-                  <i class="fa fa-truck mr-4" id="fa fa-2x"></i>
+                  <i class="fa fb fa-truck mr-4" id="fa fa-2x"></i>
                   <span class="lead align-top">Transport Management</span>
                   
                 </button>
@@ -28,7 +28,7 @@ function Dashboard(props) {
               </div>
               <div class="col-sm">
                 <button type="button" class="dbutton" id="btn-dash">
-                  <i class="fa fa-money mr-4" id="fa fa-2x"></i>
+                  <i class="fa fb fa-money mr-4" id="fa fa-2x"></i>
                   <span class="lead align-top">Financial Management</span>
                 </button>
               </div>
@@ -36,20 +36,20 @@ function Dashboard(props) {
             <div class="row">
               <div class="col-sm">
                 <Link to="/allCustomer"><button type="button" class=" dbutton" id="btn-dash">
-                  <i class="fa fa-shopping-cart pr-5"></i>
+                  <i class="fa fb fa-shopping-cart pr-5"></i>
                   <span class="lead align-top ">Order Management</span>
                 </button></Link>
               </div>
               <div class="col-sm">
                 <Link to="/allview" >
                 <button type="button" class="dbutton" id="btn-dash">
-                  <i class="fa fa-percent mr-4" id="fa fa-2x"></i>
+                  <i class="fa fb fa-percent mr-4" id="fa fa-2x"></i>
                   <span class="lead align-top">Promotion Management</span>
                 </button></Link>
               </div>
               <div class="col-sm">
                 <button type="button" class="dbutton" id="btn-dash">
-                  <i class="fa fa-cubes mr-4" id="fa fa-2x"></i>
+                  <i class="fa fb fa-cubes mr-4" id="fa fa-2x"></i>
                   <span class="lead align-top">Supply Management</span>
                 </button>
               </div>
@@ -57,21 +57,21 @@ function Dashboard(props) {
             <div class="row">
               <div class="col-sm">
                 <button type="button" class="dbutton" id="btn-dash">
-                  <i class="fa fa-line-chart mr-4" id="fa fa-2x"></i>
+                  <i class="fa fb fa-line-chart mr-4" id="fa fa-2x"></i>
                   <span class="lead align-top">Inventory Management</span>
                 </button>
               </div>
               <div class="col-sm">
               <a href="/view">
                 <button type="button" class=" dbutton" id="btn-dash">
-                  <i class="fa fa-tasks mr-4" id="fa fa-2x"></i>
+                  <i class="fa fb fa-tasks mr-4" id="fa fa-2x"></i>
                   <span class="lead align-top">Production Management</span>
                 </button></a>
               </div>
               <div class="col-sm">
                 <a href="/">
                 <button type="button" class=" dbutton" id="btn-dash">
-                  <i class="fa fa-sign-out pr-5"></i>
+                  <i class="fa fb fa-sign-out pr-5"></i>
                   <span class="lead align-top">Exit</span>
                 </button>
                 </a>
