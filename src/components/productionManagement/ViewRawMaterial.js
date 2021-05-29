ae4869b1222c82d85a5d0ad07aed081de7f08a3f
Uwishvajith <uwishvajith@gmail.com>
2021-05-30 04:37:15 +0530

    import LINK to all pages again

--- src/components/productionManagement/ViewRawMaterial.js
+++ src/components/productionManagement/ViewRawMaterial.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import MaterialTable from "material-table";
 
+
 export default function InventoryReport() {
   const [inventory, setInventory] = useState([]);
 
